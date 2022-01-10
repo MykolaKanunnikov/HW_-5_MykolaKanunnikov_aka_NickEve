984cddfd89feb2d0df28819f0a8f9997455d7987
Nick Eve <kanunnikovmykola@gmail.com>
2022-01-10 21:59:42 +0200

    Input class optimization

--- Input.java
+++ Input.java
@@ -4,40 +4,24 @@ import java.util.Scanner;
 
 public class Input {
 
-    private static String login;
-    private static String password;
-    private static String confirmPassword;
-
     static Scanner in = new Scanner(System.in);
 
     public static void logIn() {
         System.out.print("Login: ");
-        login = in.nextLine();
+        String login = in.next();
 
         System.out.print("\nPassword: ");
-        password = in.nextLine();
+        String password = in.next();
 
         System.out.print("\nConfirm password: ");
-        confirmPassword = in.nextLine();
+        String confirmPassword = in.next();
 
         try {
-            Verify.verifier(Input.getLogin(), Input.getPassword(), Input.getConfirmPassword());
+            Verify.verifier(login, password, confirmPassword);
         } catch (WrongLoginException | WrongPasswordException e) {
             e.printStackTrace();
             System.out.println("\nEach string may contain only alphanumeric characters and underscores. " +
                     "Max 20 chars allowed, including at least one letter, number, and underscore.");
         }
-    }
-
-    public static String getLogin() {
-        return login;
-    }
-
-    public static String getPassword() {
-        return password;
-    }
-
-    public static String getConfirmPassword() {
-        return confirmPassword;
     }
 }
